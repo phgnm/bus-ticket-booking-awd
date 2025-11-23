a266aa450815212456aaf3a9983226d4f8eaf268
phgnm <phuongnam710648@gmail.com>
2025-11-23 08:39:14 +0700

    fix: hide the database password

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL || 'postgresql://postgres:@BusTicket123@localhost:5432/bus_ticket_dev',
+    connectionString: process.env.DATABASE_URL || `postgresql://postgres:${process.env.POSTGRES_PASSWORD}@localhost:5432/bus_ticket_dev`,
 });
 
 module.exports = pool;
