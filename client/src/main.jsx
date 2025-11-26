75851b94b0ddae6b54792f87af52cadcf6978377
phgnm <phuongnam710648@gmail.com>
2025-11-26 10:08:25 +0700

    fix: google auth now worked normally

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { GoogleOAuthProvider } from '@react-oauth/google'; // Thêm dòng này
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import './index.css'
 import App from './App.jsx'
 
-// Thay thế bằng Client ID thật của bạn
-const GOOGLE_CLIENT_ID = "1046989758150-13pj0q144u8lt44faf9r33ief975p9nq.apps.googleusercontent.com";
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <App />
     </GoogleOAuthProvider>
   </StrictMode>,
