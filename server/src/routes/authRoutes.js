64c72772ac70dbbf9c1d139d422d65fbca3c4c59
phgnm <phuongnam710648@gmail.com>
2025-11-29 15:33:07 +0700

    feat: added password reset for backend

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
-
 const authController = require('../controllers/authController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/logout', authController.logout);
 router.post('/google-login', authController.googleLogin);
 router.post('/refresh-token', authController.refreshToken);
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/reset-password', authController.resetPassword);
 
 module.exports = router;
